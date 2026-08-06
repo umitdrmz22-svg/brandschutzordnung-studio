abedf7e2669948f7ec45534580e90c7089924d1d
umitdrmz22-svg <umitdrmz22@gmail.com>
2026-08-06 12:44:35 +0200

    Produktive Supabase-Verbindung konfigurieren

--- cloud-config.js
+++ cloud-config.js
@@ -1,9 +1,11 @@
 'use strict';
 (function(){
   const shared=window.EHS_PLATFORM_CONFIG||{};
+  const defaultSupabaseUrl='https://rqvcbjomrjccyuchxpuh.supabase.co';
+  const defaultPublishableKey='sb_publishable_iKh-ZfqV3iJpr_9b7SErEA_XhrqnSsY';
   window.BSO_CLOUD_CONFIG=Object.freeze({
-    supabaseUrl:shared.supabaseUrl||'',
-    supabasePublishableKey:shared.supabasePublishableKey||shared.supabaseAnonKey||'',
+    supabaseUrl:shared.supabaseUrl||defaultSupabaseUrl,
+    supabasePublishableKey:shared.supabasePublishableKey||shared.supabaseAnonKey||defaultPublishableKey,
     appKey:'brandschutzordnung',
     storageKey:'bso-studio-project'
   });
